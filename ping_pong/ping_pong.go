e56a766e0b3efcb6d141807347fcba019e6fcbaa
uamoti <uamoti@anche.no>
2023-12-16 23:17:46 +0200

    Update ping-pong to use goroutines

--- ping_pong/ping_pong.go
+++ ping_pong/ping_pong.go
@@ -1,26 +1,51 @@
 package main
 
 import (
-"fmt"
-"time"
+	"fmt"
+	"time"
 )
 
-func main() {
-     pp := make(chan string)
-
-     go func() {
-     for {
-     time.Sleep(1 * time.Second)
-     pp <- "PING"
-     time.Sleep(1 * time.Second)
-     pp <- "PONG"
-     }}()
-
-     fmt.Println("PRESS CTR-C TO STOP")
-     time.Sleep(3 * time.Second)
-     for {
-     p1 := <- pp
-     fmt.Println(p1)
-     }
+func Ping(c chan string) {
+	for {
+		c <- "PING"
+	}
 }
 
+func Pong(c chan string) {
+	for {
+		c <- "PONG"
+	}
+}
+
+func PrintChan(c chan string) {
+	for {
+		msg := <-c
+		fmt.Println(msg)
+		time.Sleep(1 * time.Second)
+	}
+}
+
+func main() {
+	pp := make(chan string)
+
+	// go func() {
+	// for {
+	// time.Sleep(1 * time.Second)
+	// pp <- "PING"
+	// time.Sleep(1 * time.Second)
+	// pp <- "PONG"
+	// }}()
+
+	fmt.Println("PRESS ENTER/RETURN TO STOP")
+	// time.Sleep(3 * time.Second)
+	// for {
+	// 	p1 := <-pp
+	// 	fmt.Println(p1)
+	// }
+	go Ping(pp)
+	go PrintChan(pp)
+	go Pong(pp)
+
+	var keypress string
+	fmt.Scanln(&keypress)
+}
