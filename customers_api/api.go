34e4526e4b4427371f892767b123d02e7c0e97d5
uamoti <uamoti@anche.no>
2023-12-26 00:13:00 +0200

    Add PUT method
    Refactor GetCustomer into CustomerById to handle both GET and PUT.

--- customers_api/api.go
+++ customers_api/api.go
@@ -1,16 +1,7 @@
 package main
 
-/* Create as REST API to handle customer data - CRUD.
-   Start simple, with customer type having:
-   - ID (primary key)
-   - First name
-   - Last name
-   - Address -> another struct
-   - E-mail (unique)
-   - Phone number (unique)
-   Address:
-   - Street and number
-   - Postal code */
+/* Create as REST API to handle customer data - CRUD
+On this version, I want to use only the standard packages.*/
 
 import (
 	"encoding/json"
@@ -22,17 +13,17 @@ import (
 )
 
 type Customer struct {
-	Id        int     `json:"id"`
-	FirstName string  `json:"first_name"`
-	LastName  string  `json:"last_name"`
-	Email     string  `json:"email"`
-	Phone     string  `json:"phone"`
-	Addr      Address `json:"address"`
+	Id        int     `json:"id,omitempty"`
+	FirstName string  `json:"first_name,omitempty"`
+	LastName  string  `json:"last_name,omitempty"`
+	Email     string  `json:"email,omitempty"`
+	Phone     string  `json:"phone,omitempty"`
+	Addr      Address `json:"address,omitempty"`
 }
 
 type Address struct {
-	Street   string `json:"street"`
-	Postcode string `json:"post"`
+	Street   string `json:"street,omitempty"`
+	Postcode string `json:"post,omitempty"`
 }
 
 var customers []Customer
@@ -55,16 +46,36 @@ func CustomersRootPath(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func GetCustomer(w http.ResponseWriter, r *http.Request) {
-	id := r.URL.Path
-	for _, c := range customers {
-		if cid := strconv.Itoa(c.Id); cid == id {
-			w.Header().Set("Content-Type", "application/json")
-			json.NewEncoder(w).Encode(c)
-			return
+func CustomerById(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		id := r.URL.Path
+		for _, c := range customers {
+			if cid := strconv.Itoa(c.Id); cid == id {
+				w.Header().Set("Content-Type", "application/json")
+				json.NewEncoder(w).Encode(c)
+				return
+			}
 		}
+		json.NewEncoder(w).Encode("error: customer not found")
+	case "PUT":
+		id := r.URL.Path
+		for i, c := range customers {
+			if cid := strconv.Itoa(c.Id); cid == id {
+				w.Header().Set("Content-Type", "application/json")
+				err := json.NewDecoder(r.Body).Decode(&c)
+				if err != nil {
+					http.Error(w, err.Error(), http.StatusBadRequest)
+					return
+				}
+				customers = append(customers[:i], customers[i+1:]...)
+				customers = append(customers, c)
+				json.NewEncoder(w).Encode(c)
+				return
+			}
+		}
+		json.NewEncoder(w).Encode("error: customer not found")
 	}
-	json.NewEncoder(w).Encode("error: customer not found")
 }
 
 func main() {
@@ -77,14 +88,7 @@ func main() {
 
 	jsonBites, _ := io.ReadAll(jsonFile)
 	json.Unmarshal(jsonBites, &customers)
-
-	// for _, c := range customers {
-	// 	fmt.Println(c.FirstName, c.LastName)
-	// 	fmt.Println(c.Addr)
-	// 	fmt.Println()
-	// }
-	customersPath := "/customers/"
 	http.HandleFunc("/customers", CustomersRootPath)
-	http.Handle(customersPath, http.StripPrefix(customersPath, http.HandlerFunc(GetCustomer)))
+	http.Handle("/customers/", http.StripPrefix("/customers/", http.HandlerFunc(CustomerById)))
 	http.ListenAndServe(":8000", nil)
 }
